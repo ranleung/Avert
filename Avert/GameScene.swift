9b887a105b9ee9d47a74a30d207b1833280d9b98
Randall Leung <leungran@gmail.com>
2014-10-27 13:38:31 -0700

    hero rotation on z axis working

--- Avert/GameScene.swift
+++ Avert/GameScene.swift
@@ -9,44 +9,46 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var hero: SKShapeNode!
+    var heroRotationSpeed = 5
    
     override func didMoveToView(view: SKView) {
 
-        /* Setup your scene here */
-
-        let hero = SKShapeNode(rect: CGRect(x: 0.0, y: 0.0, width: 50, height: 50))
+        //Create starting hero and position center
+        //self.hero = SKShapeNode(rect: CGRect(x: 0.0, y: 0.0, width: 50, height: 50))
+        let path = CGPathCreateWithRect(CGRect(x: 0.0, y: 0.0, width: 50, height: 50), nil)
+        self.hero = SKShapeNode(path: path, centered: true)
         
-        hero.fillColor = UIColor.blackColor()
-        hero.xScale = 0.5
-        hero.yScale = 0.5
-        hero.position = CGPoint(x: 400, y: 400)
+        self.hero.fillColor = UIColor.whiteColor()
+        self.hero.xScale = 0.5
+        self.hero.yScale = 0.5
+        self.hero.position = CGPointMake(view.frame.width/4, view.frame.height/2)
         
-        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration: 1)
+        self.hero.runAction(SKAction.repeatActionForever(action))
         
-        hero.runAction(SKAction.repeatActionForever(action))
-        
-        self.addChild(hero)
+        self.addChild(self.hero)
 
     }
+    
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-
         for touch: AnyObject in touches {
-            
-            
-            
-            
-            
-            
             
             
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
+        //rotate the hero
+//        var degreeRotation = CDouble(self.heroRotationSpeed) * M_PI / 180
+//        self.hero.zRotation -= CGFloat(degreeRotation)
         
+        
+
         
     }
 }
