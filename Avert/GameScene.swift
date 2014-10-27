cb44dbe3db9a36fa212b4f10b96c702a2abfdadb
Randall Leung <leungran@gmail.com>
2014-10-27 15:13:46 -0700

    add hero pan gesture

--- Avert/GameScene.swift
+++ Avert/GameScene.swift
@@ -12,6 +12,9 @@ class GameScene: SKScene {
     
     var hero: SKShapeNode!
     var heroRotationSpeed = 5
+    var panGestureRecognizer : UIPanGestureRecognizer!
+    var lastTouchedLocation : CGPoint!
+    var startPosition : CGPoint!
    
     override func didMoveToView(view: SKView) {
 
@@ -23,32 +26,53 @@ class GameScene: SKScene {
         self.hero.fillColor = UIColor.whiteColor()
         self.hero.xScale = 0.5
         self.hero.yScale = 0.5
-        self.hero.position = CGPointMake(view.frame.width/4, view.frame.height/2)
+        self.hero.position = CGPointMake(view.frame.width/2, view.frame.height/2)
         
         let action = SKAction.rotateByAngle(CGFloat(M_PI), duration: 1)
         self.hero.runAction(SKAction.repeatActionForever(action))
-        
         self.addChild(self.hero)
 
+        self.panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        self.view?.addGestureRecognizer(panGestureRecognizer)
     }
-    
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            
-            
+
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        //rotate the hero
-//        var degreeRotation = CDouble(self.heroRotationSpeed) * M_PI / 180
-//        self.hero.zRotation -= CGFloat(degreeRotation)
-        
-        
 
-        
+    override func update(currentTime: CFTimeInterval) {
+    
+    }
+
+    func handlePan (panGestureRecognizer: UIPanGestureRecognizer) {
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            self.startPosition = self.hero.position
+        }
+        if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            //let translatedPoint =
+            var touchLocation = self.panGestureRecognizer.translationInView(self.view!)
+            touchLocation = CGPointMake(touchLocation.x, -touchLocation.y)
+            var newLocation = CGPointMake(startPosition.x + touchLocation.x, startPosition.y + touchLocation.y)
+//            touchLocation = self.convertPointFromView(touchLocation)
+            self.hero.position = newLocation
+//            var xDistance = lastTouchedLocation.x - touchLocation.x
+//            var yDistance = lastTouchedLocation.y - touchLocation.y
+//            if touchLocation.x < lastTouchedLocation.x {
+//                xDistance = xDistance * -1
+//            }
+//            if touchLocation.y < lastTouchedLocation.y {
+//                yDistance = yDistance * -1
+//            }
+//            var currentPosition = touchLocation
+//            self.hero.position = currentPosition
+//            self.lastTouchedLocation = touchLocation
+            println("Changed \(self.panGestureRecognizer.locationInView(self.view))")
+        }
+        if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            println("Ended \(self.panGestureRecognizer.locationInView(self.view))")
+        }
     }
 }
