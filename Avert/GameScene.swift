c2bb15fae96d0534b89c5eb5919cb86d1b2fdf1e
Reid Weber <reid_weber@hotmail.com>
2014-10-27 14:11:49 -0700

    Captured Touch on Play Button

--- Avert/GameScene.swift
+++ Avert/GameScene.swift
@@ -10,12 +10,24 @@ import SpriteKit
 
 class GameScene: SKScene {
    
+    var menuNode: MenuScreenNode?
+    var showMenu = true
+    
     override func didMoveToView(view: SKView) {
-        
+        self.menuNode = MenuScreenNode(scene: self)
+        self.addChild(self.menuNode!)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
+        if self.showMenu == true {
+            for touch in touches {
+                var nodeAtTouch = self.menuNode?.nodeAtPoint(touch.locationInNode(self.menuNode))
+                if nodeAtTouch?.name == "PlayButton" {
+                    println("PlayButton Touched")
+                }
+            }
+        }
         
     }
    
--- Avert/MenuScreenNode.swift
+++ Avert/MenuScreenNode.swift
@@ -10,6 +10,45 @@ import UIKit
 import SpriteKit
 
 class MenuScreenNode: SKNode {
-    var playButton: SKNode?
-    var helpButton: SKNode?
+    var playButton: SKLabelNode!
+    var helpButton: SKLabelNode!
+    var titleLabel: SKLabelNode!
+    //var gameCenterButton: SKSpriteNode?
+    
+    init(scene: SKScene) {
+        super.init()
+
+        self.titleLabel = SKLabelNode(text: "Avert")
+        self.titleLabel.fontName = "Chalkduster"
+        self.titleLabel.fontSize = 50
+        
+        self.titleLabel.position = CGPoint(x: CGRectGetMidX(scene.frame), y: CGRectGetMidY(scene.frame))
+        self.addChild(self.titleLabel)
+        
+        self.playButton = SKLabelNode(text: "Play")
+        self.playButton.fontName = "Chalkduster"
+        self.playButton.fontSize = 20
+        self.playButton.name = "PlayButton"
+        
+        self.playButton.position = CGPoint(x: CGRectGetMidX(scene.frame) - self.playButton.frame.width, y: CGRectGetMidY(scene.frame) - self.titleLabel.frame.height)
+        self.addChild(self.playButton)
+        
+        self.helpButton = SKLabelNode(text: "Help")
+        self.helpButton.fontName = "Chalkduster"
+        self.helpButton.fontSize = 20
+        self.helpButton.name = "HelpButton"
+        
+        
+        self.helpButton.position = CGPoint(x: CGRectGetMidX(scene.frame) + (self.titleLabel.frame.width / 3) , y: CGRectGetMidY(scene.frame) - self.titleLabel.frame.height)
+        self.addChild(self.helpButton)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    func recieveTouchEvent(touches: NSSet) {
+        for touch in touches {
+            
+        }
+    }
 }
