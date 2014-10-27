cb8ea507d67bef8ea72e833651d96600e6fbcc4b
Reid Weber <reid_weber@hotmail.com>
2014-10-27 12:52:46 -0700

    Added Menu Node Subclass

---
+++ Avert/MenuScreenNode.swift
@@ -0,0 +1,13 @@
+//
+//  MenuScreenNode.swift
+//  Avert
+//
+//  Created by Reid Weber on 10/27/14.
+//  Copyright (c) 2014 Randall. All rights reserved.
+//
+
+import UIKit
+
+class MenuScreenNode: SKNode {
+   
+}
