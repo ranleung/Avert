5c178741be1f0a10d6160288147ecaccd2ed0a12
Will Richman <wrichman@gmail.com>
2014-10-28 16:05:24 -0700

    Created Powerup class.

---
+++ Powerup.swift
@@ -0,0 +1,25 @@
+//
+//  Powerup.swift
+//  Avert
+//
+//  Created by William Richman on 10/28/14.
+//  Copyright (c) 2014 Randall. All rights reserved.
+//
+
+import SpriteKit
+
+class Powerup: Shape {
+   
+    class func spawnPowerup (team: ShapeTeam, scene: SKScene) -> Powerup {
+        let powerup = Powerup(side: randomSide(), team: team, scene: scene)
+        powerup.spawnSprite()
+        powerup.sprite?.color = UIColor.greenColor()
+        return powerup
+    }
+    
+    class func randomSide() -> OriginSide {
+        let randomIndex = arc4random() % 4
+        return Powerup.OriginSide.allValues[Int(randomIndex)]
+    }
+    
+}
