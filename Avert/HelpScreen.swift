55ec75880ef60849f5ada4c0287b8071b51c6486
mbrightbill <brightbill.mt@gmail.com>
2014-10-27 17:09:53 -0700

    Added help rules texts

--- Avert/HelpScreen.swift
+++ Avert/HelpScreen.swift
@@ -16,6 +16,10 @@ class HelpScreen: SKNode {
     var playerLabel: SKLabelNode!
     var enemyLabel: SKLabelNode!
     var friendLabel: SKLabelNode!
+    var rulesTitleLabel: SKLabelNode!
+    var firstRuleLabel: SKLabelNode!
+    var secondRuleLabel: SKLabelNode!
+    var thirdRuleLabel: SKLabelNode!
     
     init(scene: SKScene) {
         super.init()
@@ -45,13 +49,36 @@ class HelpScreen: SKNode {
         self.playerLabel.position = CGPoint(x: scene.frame.width * 0.2 - ((self.friendLabel.frame.width / 2) - (self.playerLabel.frame.width / 2)), y: scene.frame.height * 0.25)
         self.addChild(self.playerLabel)
         
-        // (self.friendlabel.frame.width / 2) - (self.playerLabel.frame.width / 2)
         
         self.enemyLabel = SKLabelNode(text: " - Enemies")
         self.enemyLabel.fontName = "Chalkduster"
         self.enemyLabel.fontSize = 20
         self.enemyLabel.position = CGPoint(x: scene.frame.width * 0.2 - ((self.friendLabel.frame.width / 2) - (self.enemyLabel.frame.width / 2)), y: scene.frame.height * 0.5)
         self.addChild(self.enemyLabel)
+        
+        self.rulesTitleLabel = SKLabelNode(text: "Rules:")
+        self.rulesTitleLabel.fontName = "Chalkduster"
+        self.rulesTitleLabel.fontSize = 20
+        self.rulesTitleLabel.position = CGPoint(x: scene.frame.width * 0.2 + self.friendLabel.frame.width * 1.5, y: scene.frame.height - self.titleLabel.frame.height)
+        self.addChild(rulesTitleLabel)
+        
+        self.firstRuleLabel = SKLabelNode(text: "Blah")
+        self.firstRuleLabel.fontName = "Chalkduster"
+        self.firstRuleLabel.fontSize = 20
+        self.firstRuleLabel.position = CGPoint(x: scene.frame.width * 0.2 + self.friendLabel.frame.width * 1.5, y: scene.frame.height * 0.75)
+        self.addChild(firstRuleLabel)
+        
+        self.secondRuleLabel = SKLabelNode(text: "Blah")
+        self.secondRuleLabel.fontName = "Chalkduster"
+        self.secondRuleLabel.fontSize = 20
+        self.secondRuleLabel.position = CGPoint(x: scene.frame.width * 0.2 + self.friendLabel.frame.width * 1.5, y: scene.frame.height * 0.5)
+        self.addChild(secondRuleLabel)
+        
+        self.thirdRuleLabel = SKLabelNode(text: "a;lsdfj")
+        self.thirdRuleLabel.fontName = "Chalkduster"
+        self.thirdRuleLabel.fontSize = 20
+        self.thirdRuleLabel.position = CGPoint(x: scene.frame.width * 0.2 + self.friendLabel.frame.width * 1.5, y: scene.frame.height * 0.25)
+        self.addChild(thirdRuleLabel)
         
     }
 
