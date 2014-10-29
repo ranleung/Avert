74f88ce696a4787b37dd545252abfc8a2753cc47
Jeff Chavez <jeffchavez8588@gmail.com>
2014-10-29 11:32:21 -0700

    tweaked square colors

--- Avert/Shape.swift
+++ Avert/Shape.swift
@@ -59,9 +59,9 @@ class Shape {
         
         switch self.team {
         case .Friend:
-            sprite.color = UIColor.blueColor()
+            sprite.color = UIColor(red: 0, green: 144/255, blue: 1, alpha: 1)
         case .Enemy:
-            sprite.color = UIColor.orangeColor()
+            sprite.color = UIColor(red: 1, green: 150/255, blue: 0, alpha: 1)
         }
         
         var destination: CGPoint!
