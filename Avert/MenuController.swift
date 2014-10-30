df4ba20497cab6f0a97bce9f77b3bfa887cdaec5
mbrightbill <brightbill.mt@gmail.com>
2014-10-30 10:27:45 -0700

    Adjusted pause/play buttons to final position

--- Avert/MenuController.swift
+++ Avert/MenuController.swift
@@ -29,8 +29,8 @@ class MenuController {
         
         self.pauseButton = SKSpriteNode(imageNamed: "PauseButton")
         self.resumeButton = SKSpriteNode(imageNamed: "PlayButton")
-        self.pauseButton?.position = CGPoint(x: scene.frame.width - self.pauseButton!.frame.width/3, y: scene.frame.height - self.pauseButton!.frame.height/4)
-        self.resumeButton?.position = CGPoint(x: scene.frame.width - self.pauseButton!.frame.width/3, y: scene.frame.height - self.pauseButton!.frame.height/4)
+        self.pauseButton?.position = CGPoint(x: scene.frame.width - self.pauseButton!.frame.width/2, y: scene.frame.height - self.pauseButton!.frame.height/3)
+        self.resumeButton?.position = CGPoint(x: scene.frame.width - self.pauseButton!.frame.width/2, y: scene.frame.height - self.pauseButton!.frame.height/3)
         self.pauseButton?.name = "PauseButton"
         self.resumeButton?.name = "PlayButton"
         self.pausedLabel = SKLabelNode(text: "Paused")
@@ -65,10 +65,10 @@ class MenuController {
         self.soundOn.zPosition = 2.0
         self.soundOff.zPosition = 2.0
         
-        self.pauseButton?.xScale = 0.2
-        self.pauseButton?.yScale = 0.2
-        self.resumeButton?.xScale = 0.2
-        self.resumeButton?.yScale = 0.2
+        self.pauseButton?.xScale = 0.3
+        self.pauseButton?.yScale = 0.3
+        self.resumeButton?.xScale = 0.3
+        self.resumeButton?.yScale = 0.3
     }
     
     func addHelpScreen(scene: GameScene) {
